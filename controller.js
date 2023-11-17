5da0bf27dabbd642fb164c93c78bda6c93730c8a
Mio Lundqvist <mio.lundqvist@MioMacBook-Pro.local>
2023-11-17 14:33:45 +0100

    first project structure

---
+++ controller.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const app = new express
+const portNr = 8080
+
+const bodyParser = require("body-parser")
+const fs = require("fs")
+const res = require("express/lib/response")
